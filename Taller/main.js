0b86146b3f05e85354c2e4a50287ab4c80e5bda2
Camilo House <juancamilog9911@gmail.com>
2023-10-24 12:23:08 -0500

    mejorando codigo de javaScript

--- Taller/main.js
+++ Taller/main.js
@@ -1,40 +1,96 @@
-let precios = "";
-const productoHamburguesa = document.getElementById("Producto1");
-const productoPerro = document.getElementById("Producto2");
-const cantidad = parseInt(document.getElementById("cantidad").value);
+let precios = [];
 let subtotal = 0;
-
 const productos = [
     {
         id: "Producto1",
         nombre: "Hamburguesa",
-        precio : 50000,
+        precio: 50000,
     },
     {
         id: "Producto2",
-        nombre: "perro",
-        precio : 75000,
+        nombre: "Perro",
+        precio: 75000,
     }
 ];
 
-const precioProductos = document.getElementById("precioProductos");
+function calcularProductos() {
+    precios = [];
+    subtotal = 0;
+    const checked = document.querySelectorAll("input[type=checkbox]:checked");
 
-const pedido = [];
+    for (let i = 0; i < checked.length; i++) {
+        const productoId = checked[i].id;
+        const producto = productos.find(p => p.id === productoId);
 
-function calcularProductos(){
-
-    let checked = document.querySelectorAll("input[type=checkbox]:checked");
-
-    if( checked ) {
-        for (let i = 0; i < checked.length; i++) {
-            precios += '<li>'+ productos[i].nombre + ' : ' + productos[i].precio +' </li>';
-            document.getElementById("listaPedidos").innerHTML = precios;
-        
-        subtotal +=  productos[i].precio;
-        console.log ("pepe");
+        if (producto) {
+            precios.push(producto.nombre + ' : ' + producto.precio);
+            subtotal += producto.precio;
         }
-    }else{
-        console.log ("pepe");
     }
 
+    document.getElementById("listaPedidos").innerHTML = precios.join('<br>');
+}
+
+function calcularDescuento() {
+    let descuentoMensaje = "";
+
+    if (subtotal > 50000 && subtotal < 100000) {
+        subtotal = subtotal * 0.90;
+        descuentoMensaje = "Descuento del 10%";
+    } else if (subtotal >= 100000 && subtotal < 200000) {
+        subtotal = subtotal * 0.85;
+        descuentoMensaje = "Descuento del 15%";
+    } else if (subtotal >= 200000) {
+        subtotal = subtotal * 0.80;
+        descuentoMensaje = "Descuento del 20%";
+    } else {
+        descuentoMensaje = "Sin descuento";
+    }
+
+    document.getElementById("PrecioTotal").innerHTML = descuentoMensaje + "<br>Total: " + subtotal;
+}
+
+function ProductosNuevos() {
+    document.getElementById("principal").style.display = "none";
+    document.getElementById("ProductosNuevos").style.display = "block";
+    document.getElementById("regresar").style.display = "block";
+}
+
+function regresarPrincipal() {
+    document.getElementById("principal").style.display = "block";
+    document.getElementById("ProductosNuevos").style.display = "none";
+    document.getElementById("regresar").style.display = "none";
+}
+
+function agregarProductoNuevo() {
+    const nombreProductoNuevo = document.getElementById("nombreProductoNuevo").value;
+    const precioProductoNuevo = parseInt(document.getElementById("precioProductoNuevo").value);
+
+    if (nombreProductoNuevo && !isNaN(precioProductoNuevo) && precioProductoNuevo > 0) {
+        const nuevoProductoId = "Producto" + (productos.length + 1);
+        const nuevoProducto = {
+            id: nuevoProductoId,
+            nombre: nombreProductoNuevo,
+            precio: precioProductoNuevo,
+        };
+
+        productos.push(nuevoProducto);
+
+        const productosActuales = document.getElementById("productosActuales");
+        const label = document.createElement("label");
+        label.setAttribute("for", nuevoProductoId);
+        label.textContent = nombreProductoNuevo;
+
+        const input = document.createElement("input");
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("id", nuevoProductoId);
+        input.setAttribute("value", precioProductoNuevo);
+
+        productosActuales.appendChild(label);
+        productosActuales.appendChild(input);
+
+        alert("Producto agregado con éxito.");
+    } else {
+        alert("Por favor, ingrese un nombre y un precio válido para el nuevo producto.");
+    }
 }
